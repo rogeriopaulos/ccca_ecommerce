2a088107b9b8700568375c84bfdb9bcfb05b84bd
Rogerio Paulo <rogeriopaulos@gmail.com>
2022-05-08 15:09:36 -0300

    Correção diversa

--- src/frete.py
+++ src/frete.py
@@ -4,12 +4,13 @@ from src.item import Item
 class Frete:
     def __init__(self):
         self._total = 0
-        self._DISTANCE = 1000
-        self._FACTOR = 100
+        self._DISTANCIA = 1000
+        self._FATOR = 100
+        self._FRETE_MINIMO = 10
 
     def adicionar_item(self, item: Item, quantidade: int):
-        frete = item.calcular_volume() * self._DISTANCE * (item.calcular_densidade()/self._FACTOR)
+        frete = item.calcular_volume() * self._DISTANCIA * (item.calcular_densidade()/self._FATOR)
         self._total += frete * quantidade
 
     def calcular_total(self):
-        return 10 if self._total > 0 and self._total < 10 else self._total
+        return self._FRETE_MINIMO if self._total > 0 and self._total < self._FRETE_MINIMO else self._total
